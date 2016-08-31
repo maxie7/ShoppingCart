874270c023b61142e2ada784840cf32cec4e64c4
Max Kos <maxie7@protonmail.com>
2016-08-31 13:09:41 +0300

    Update adding product test

--- spec/features/shopping_cart/customer_can_add_product_to_order_spec.rb
+++ spec/features/shopping_cart/customer_can_add_product_to_order_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
-feature 'Add book' do
-  given(:customer) {FactoryGirl.create(:customer)}
+module ShoppingCart
+  feature 'Add book' do
+    given(:customer) {FactoryGirl.create(:customer)}
 
-  before do
-    login_as customer
-    visit book_path(product)
-  end
-  given!(:product) {FactoryGirl.create(:product)}
+    before do
+      login_as customer
+      visit book_path(product)
+    end
+    given!(:product) {FactoryGirl.create(:product)}
 
-  scenario 'Customer add book to cart' do
-    fill_in 'orders[quantity]', with: 3
-    click_button 'Add to Cart'
+    scenario 'Customer add book to cart' do
+      fill_in 'orders[quantity]', with: 3
+      click_button 'Add to Cart'
 
-    expect(page).not_to have_content 'EMPTY'
-    expect(page).to have_content '(3)'
-    expect(page).to have_content number_to_currency product.price*3
+      expect(page).not_to have_content 'EMPTY'
+      expect(page).to have_content '(3)'
+      expect(page).to have_content number_to_currency product.price*3
+    end
   end
 end
