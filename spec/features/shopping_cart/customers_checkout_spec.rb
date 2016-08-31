4f95d12adcbf9e3d484138f02d056267005519d2
Max Kos <maxie7@protonmail.com>
2016-08-31 13:08:13 +0300

    Refactor chec kout updates

--- spec/features/shopping_cart/customers_checkout_spec.rb
+++ spec/features/shopping_cart/customers_checkout_spec.rb
@@ -1,68 +1,70 @@
 require 'rails_helper'
 
 include OrderStepHelper
-feature 'fill order step by step' do
-  given(:customer) {FactoryGirl.create(:customer)}
-  given!(:order) {FactoryGirl.create(:order,customer_id:customer.id, state:'in_progress', total_price:10 , delivery_price:5)}
-  given!(:product){FactoryGirl.create(:product)}
-  given!(:country){FactoryGirl.create(:country)}
-  given!(:delivery) {FactoryGirl.create(:delivery_method)}
-  before do
-    login_as customer
-  end
+module ShoppingCart
+  feature 'fill order step by step' do
+    given(:customer) {FactoryGirl.create(:customer)}
+    given!(:order) {FactoryGirl.create(:order,customer_id:customer.id, state:'in_progress', total_price:10 , delivery_price:5)}
+    given!(:product){FactoryGirl.create(:product)}
+    given!(:country){FactoryGirl.create(:country)}
+    given!(:delivery) {FactoryGirl.create(:delivery_method)}
+    before do
+      login_as customer
+    end
 
-  scenario 'customer view address step' do
-    visit order_step_path(:address)
-    fill_address
-    click_button 'SAVE AND CONTINUE'
-    expect(current_path).to eq(order_step_path(:delivery))
-  end
+    scenario 'customer view address step' do
+      visit order_step_path(:address)
+      fill_address
+      click_button 'SAVE AND CONTINUE'
+      expect(current_path).to eq(order_step_path(:delivery))
+    end
 
-  scenario 'customer view delivery step' do
-    visit order_step_path(:delivery)
-    choose delivery.name
-    click_button 'SAVE AND CONTINUE'
-    expect(current_path).to eq(order_step_path(:payment))
+    scenario 'customer view delivery step' do
+      visit order_step_path(:delivery)
+      choose delivery.name
+      click_button 'SAVE AND CONTINUE'
+      expect(current_path).to eq(order_step_path(:payment))
 
-  end
+    end
 
-  scenario 'customer view credit_card step' do
-    order.update(billing_address: FactoryGirl.create(:address),
-                 shipping_address: FactoryGirl.create(:address),
-                 delivery_method: FactoryGirl.create(:delivery_method))
-    visit order_step_path(:payment)
-    fill_credit_card
-    click_button 'SAVE AND CONTINUE'
-    expect(current_path).to eq(order_step_path(:confirm))
-  end
-
-
-  scenario 'customer view confirm step' do
-    fill_all_fields
-    visit order_step_path(:confirm)
-    expect(page).to have_content order.billing_address.address
-    expect(page).to have_content order.shipping_address.address
-    expect(page).to have_content order.delivery_method.name
-    expect(page).to have_content order.credit_card.expiration_year
-    expect(page).to have_content order.credit_card.number.to_s.slice(-4..-1)
-  end
-
-  scenario 'customer view complete step' do
-    fill_all_fields
-    order.checkout!
-    visit order_step_path(:complete)
-    expect(page).to have_content order.billing_address.address
-    expect(page).to have_content order.shipping_address.address
-    expect(page).to have_content order.delivery_method.name
-    expect(page).to have_content order.credit_card.expiration_year
-    expect(page).to have_content order.credit_card.number.to_s.slice(-4..-1)
-  end
+    scenario 'customer view credit_card step' do
+      order.update(billing_address: FactoryGirl.create(:address),
+                   shipping_address: FactoryGirl.create(:address),
+                   delivery_method: FactoryGirl.create(:delivery_method))
+      visit order_step_path(:payment)
+      fill_credit_card
+      click_button 'SAVE AND CONTINUE'
+      expect(current_path).to eq(order_step_path(:confirm))
+    end
 
 
-  def fill_all_fields
-    order.update(billing_address: FactoryGirl.create(:address),
-                 shipping_address: FactoryGirl.create(:address),
-                 delivery_method: create(:delivery_method),
-                 credit_card: FactoryGirl.create(:credit_card))
+    scenario 'customer view confirm step' do
+      fill_all_fields
+      visit order_step_path(:confirm)
+      expect(page).to have_content order.billing_address.address
+      expect(page).to have_content order.shipping_address.address
+      expect(page).to have_content order.delivery_method.name
+      expect(page).to have_content order.credit_card.expiration_year
+      expect(page).to have_content order.credit_card.number.to_s.slice(-4..-1)
+    end
+
+    scenario 'customer view complete step' do
+      fill_all_fields
+      order.checkout!
+      visit order_step_path(:complete)
+      expect(page).to have_content order.billing_address.address
+      expect(page).to have_content order.shipping_address.address
+      expect(page).to have_content order.delivery_method.name
+      expect(page).to have_content order.credit_card.expiration_year
+      expect(page).to have_content order.credit_card.number.to_s.slice(-4..-1)
+    end
+
+
+    def fill_all_fields
+      order.update(billing_address: FactoryGirl.create(:address),
+                   shipping_address: FactoryGirl.create(:address),
+                   delivery_method: create(:delivery_method),
+                   credit_card: FactoryGirl.create(:credit_card))
+    end
   end
 end
