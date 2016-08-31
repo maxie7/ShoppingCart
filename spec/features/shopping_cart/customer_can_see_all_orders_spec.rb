57499138137d056c4c065ce57640045a7cd6a1d2
Max Kos <maxie7@protonmail.com>
2016-08-31 13:14:27 +0300

    Change orders spec

--- spec/features/shopping_cart/customer_can_see_all_orders_spec.rb
+++ spec/features/shopping_cart/customer_can_see_all_orders_spec.rb
@@ -1,24 +1,27 @@
-feature 'Add book' do
-  given(:customer) {FactoryGirl.create(:customer)}
+require 'rails_helper'
 
-  before do
-    login_as customer
-    visit orders_path
+module ShoppingCart
+  feature 'Add book' do
+    given(:customer) {FactoryGirl.create(:customer)}
+
+    before do
+      login_as customer
+      visit orders_path
+    end
+    given!(:product) {FactoryGirl.create(:product, id:1, price: 10)}
+    scenario 'Customer without any order' do
+      expect(page).to have_content("Lets go shopping!")
+    end
+
+    scenario 'Customer can see order in progress' do
+      visit book_path(1)
+      click_button 'Add to Cart'
+      visit orders_path
+
+      expect(page).not_to have_content("Lets go shopping!")
+      expect(page).to have_content("#{product.title}")
+      expect(page).to have_content("#{product.price}")
+    end
+
   end
-  given!(:product) {FactoryGirl.create(:product,id:1,price: 10)}
-  scenario 'Customer without any order' do
-    expect(page).to have_content("Lets go shopping!")
-  end
-
-
-  scenario 'Customer can see order in progress' do
-    visit book_path(1)
-    click_button 'Add to Cart'
-    visit orders_path
-
-    expect(page).not_to have_content("Lets go shopping!")
-    expect(page).to have_content("#{product.title}")
-    expect(page).to have_content("#{product.price}")
-  end
-
 end
